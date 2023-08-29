64046236d74aa27fc7407c3658fb907df6cf16b0
SilkyFowl <16532218+SilkyFowl@users.noreply.github.com>
2023-08-29 18:35:30 +0900

    v0.0.3

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # SilkyFowl.Avalonia.FuncUI.LiveView.Demo
+
 SilkyFowl.Avalonia.FuncUI.LiveView Demo
+
+## Usage
+
+```sh
+gh repo clone SilkyFowl/SilkyFowl.Avalonia.FuncUI.LiveView.Demo
+# or
+# git clone https://github.com/SilkyFowl/SilkyFowl.Avalonia.FuncUI.LiveView.Demo.git
+
+cd SilkyFowl.Avalonia.FuncUI.LiveView.Demo
+
+dotnet tool restore
+dotnet tool install SilkyFowl.Avalonia.FuncUI.LiveView.Analyzer --version 0.0.3 --tool-path analyzers
+dotnet restore
+
+code .
+```
+
+set debug setting `.NET Core Launch (Preview)`.
+
+![Alt text](/assets/set-debug-setting.png)
+
+Launch debugger!!
---
+++ src/App/Program.fs
@@ -0,0 +1,90 @@
+﻿namespace CounterApp
+
+open Avalonia
+open Avalonia.Controls.ApplicationLifetimes
+open Avalonia.Themes.Fluent
+open Avalonia.FuncUI.Hosts
+open Avalonia.Controls
+open Avalonia.FuncUI
+open Avalonia.FuncUI.DSL
+open Avalonia.Layout
+
+open Avalonia.FuncUI.LiveView
+open Avalonia.FuncUI.LiveView.Core.Types
+
+module Main =
+    [<LivePreview>]
+    let view () =
+        Component(fun ctx ->
+            let state = ctx.useState 0
+
+            DockPanel.create [
+                DockPanel.children [
+                    Button.create [
+                        Button.dock Dock.Bottom
+                        Button.onClick (fun _ -> state.Set(state.Current - 1))
+                        Button.content "-"
+                        Button.horizontalAlignment HorizontalAlignment.Stretch
+                        Button.horizontalContentAlignment HorizontalAlignment.Center
+                    ]
+                    Button.create [
+                        Button.dock Dock.Bottom
+                        Button.onClick (fun _ -> state.Set(state.Current + 1))
+                        Button.content "+"
+                        Button.horizontalAlignment HorizontalAlignment.Stretch
+                        Button.horizontalContentAlignment HorizontalAlignment.Center
+                    ]
+                    TextBlock.create [
+                        TextBlock.dock Dock.Top
+                        TextBlock.fontSize 48.0
+                        TextBlock.verticalAlignment VerticalAlignment.Center
+                        TextBlock.horizontalAlignment HorizontalAlignment.Center
+                        TextBlock.text (string state.Current)
+                    ]
+                ]
+            ])
+
+type MainWindow() =
+    inherit HostWindow()
+
+    do
+        base.Title <- "Counter Example"
+        base.Content <- Main.view ()
+
+module LiveView =
+    let enabled =
+        match System.Environment.GetEnvironmentVariable("FUNCUI_LIVEPREVIEW") with
+        | null -> false
+        | "1" -> true
+        | _ -> false
+
+type App() =
+    inherit Application()
+
+    override this.Initialize() =
+        this.Styles.Add(FluentTheme())
+        this.RequestedThemeVariant <- Styling.ThemeVariant.Dark
+
+    override this.OnFrameworkInitializationCompleted() =
+        match this.ApplicationLifetime with
+        | :? IClassicDesktopStyleApplicationLifetime as desktopLifetime ->
+            desktopLifetime.MainWindow <-
+                if LiveView.enabled then
+                    LiveViewWindow() :> Window
+                else
+                    MainWindow()
+        | _ -> ()
+
+#if DEBUG
+        this.AttachDevTools()
+#endif
+
+module Program =
+
+    [<EntryPoint>]
+    let main (args: string[]) =
+        AppBuilder
+            .Configure<App>()
+            .UsePlatformDetect()
+            .UseSkia()
+            .StartWithClassicDesktopLifetime(args)
